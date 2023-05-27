b08d8fb3523737b2e51a04cc31f49ebe55723be4
Zeem <1144980542@qq.com>
2023-05-27 12:02:20 +0800

    第三次尝试直线绘制

--- tinyrenderer/src/main.cpp
+++ tinyrenderer/src/main.cpp
@@ -5,11 +5,30 @@ const TGAColor red = TGAColor(255, 0, 0, 255);
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 {
+    bool steep = false;
+    if (std::abs(x0 - x1) < std::abs(y0 - y1))
+    { // if the line is steep, we transpose the image
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1)
+    { // make it left−to−right
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
     for (int x = x0; x <= x1; x++)
     {
         float t = (x - x0) / (float)(x1 - x0);
         int y = y0 * (1. - t) + y1 * t;
-        image.set(x, y, color);
+        if (steep)
+        {
+            image.set(y, x, color); // if transposed, de−transpose
+        }
+        else
+        {
+            image.set(x, y, color);
+        }
     }
 }
 
