8baa2f0a85ab19863acaa6a2e434134bfbcca6af
Zeem <1144980542@qq.com>
2023-05-27 15:34:18 +0800

    通过水平切割画出三角形的下半部分

--- tinyrenderer/src/main.cpp
+++ tinyrenderer/src/main.cpp
@@ -49,9 +49,17 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
         std::swap(t0, t2);
     if (t1.y > t2.y)
         std::swap(t1, t2);
-    line(t0, t1, image, green);
-    line(t1, t2, image, green);
-    line(t2, t0, image, red);
+    int total_height = t2.y - t0.y;
+    for (int y = t0.y; y <= t1.y; y++)
+    {
+        int segment_height = t1.y - t0.y + 1;
+        float alpha = (float)(y - t0.y) / total_height;
+        float beta = (float)(y - t0.y) / segment_height; // be careful with divisions by zero
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = t0 + (t1 - t0) * beta;
+        image.set(A.x, y, red);
+        image.set(B.x, y, green);
+    }
 }
 
 int main(int argc, char **argv)
