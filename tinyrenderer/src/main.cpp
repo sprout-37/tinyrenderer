98bf9f116d9e3be451d1b8937f231e0404ffc4d9
Zeem <1144980542@qq.com>
2023-05-27 12:00:05 +0800

    第二次尝试直线绘制

--- tinyrenderer/src/main.cpp
+++ tinyrenderer/src/main.cpp
@@ -5,10 +5,10 @@ const TGAColor red = TGAColor(255, 0, 0, 255);
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 {
-    for (float t = 0.; t < 1.; t += .01)
+    for (int x = x0; x <= x1; x++)
     {
-        int x = x0 + (x1 - x0) * t;
-        int y = y0 + (y1 - y0) * t;
+        float t = (x - x0) / (float)(x1 - x0);
+        int y = y0 * (1. - t) + y1 * t;
         image.set(x, y, color);
     }
 }
@@ -17,7 +17,9 @@ int main(int argc, char **argv)
 {
     TGAImage image(300, 300, TGAImage::RGB);
 
-    line(100, 100, 200, 200, image, white);
+    line(13, 20, 80, 40, image, white);
+    line(20, 13, 40, 80, image, red);
+    line(80, 40, 13, 20, image, red);
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
 
